89179a6ded2f537fb9a30b3f6de94f5b036e66b1
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-04 00:52:58 +0530

    fix:hydration err. fixed

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { BackgroundGradientAnimation } from "./_components/background-animation";
 import Logo from "./_components/logo";
@@ -7,8 +8,8 @@ const BackgroundGradientAnimationDemo = () => {
     <BackgroundGradientAnimation>
       <Logo/>
       <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-6xl">
-        <p className="bg-clip-text drop-shadow-2xl bg-gradient-to-b from-yellow-500/100 to-cyan-500">
-        SketchSync For Collaborative <p className="bg-gradient-to-b bg-clip-text text-transparent from-yellow-500/100 to-cyan-500">Teams</p>
+        <p className="bg-clip-text drop-shadow-2xl bg-gradient-to-b from-yellow-500/100 to-cyan-500 flex flex-col">
+        SketchSync For Collaborative <span className="bg-gradient-to-b bg-clip-text text-transparent from-yellow-300/100 to-cyan-200/10">Teams</span>
         </p>
       </div>
     </BackgroundGradientAnimation>
