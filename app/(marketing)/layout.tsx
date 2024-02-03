59563f7bfe3ddd241b4abff221a06ca9d5988e41
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-03 22:56:22 +0530

    first commit

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+const MarketingLayout = ({
+    children
+}:{
+    children:React.ReactNode
+}) => {
+    return ( 
+        <div>
+        {children}
+        </div>
+    );
+}
+
+export default MarketingLayout;
