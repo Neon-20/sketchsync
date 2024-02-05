2f5645bad6f73ee4f877e4f8de374e2a2acc7119
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-05 22:51:18 +0530

    style:small change

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -7,7 +7,7 @@ const BackgroundGradientAnimationDemo = () => {
   return (
     <BackgroundGradientAnimation>
       <Logo/>
-      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-6xl">
+      <div className="absolute z-100 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-6xl">
         <p className="bg-clip-text drop-shadow-2xl bg-gradient-to-b from-yellow-500/100 to-cyan-500 flex flex-col">
         SketchSync For Collaborative <span className="bg-gradient-to-b bg-clip-text text-transparent from-yellow-300/100 to-cyan-200/10">Teams</span>
         </p>
