a80e47af116270da4d5d982f476758507973f350
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-11 14:08:34 +0530

    style:minor changes

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 const Logo = () => {
     return ( 
-        <div className="hover:opacity-200/10 transition z-50
-        items-center gap-x-2 hidden backdrop-blur-sm p-2 md:flex cursor-pointer">
+        <div className="transition z-50 ease-in-out duration-300
+        items-center text-transparent bg-clip-text gap-x-2 p-4 flex cursor-pointer">
             <Image
             alt = "Logo"
             src = "/logo.png"
             width="50"
             height="50"
-            className="object-cover cursor-pointer"
+            className="object-cover cursor-pointer flex md:flex"
             />
             <Link href="">
             <p className="z-100 text-2xl text-white font-semibold 
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,7 +8,7 @@ const BackgroundGradientAnimationDemo = () => {
     <BackgroundGradientAnimation>
       <Logo/>
       <div className="absolute z-100 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-6xl">
-        <p className="bg-clip-text drop-shadow-2xl bg-gradient-to-b from-yellow-500/100 to-cyan-500 flex flex-col">
+        <p className="bg-clip-text drop-shadow-2xl bg-gradient-to-b from-yellow-500/100 to-cyan-500 flex flex-col backdrop-blur-sm z-50">
         SketchSync For Collaborative <span className="bg-gradient-to-b bg-clip-text text-transparent from-yellow-300/100 to-cyan-200/10">Teams</span>
         </p>
       </div>
