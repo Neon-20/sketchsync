2764f8698ce52e925a56c06feb198aaf0c4d75bb
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-14 01:36:37 +0530

    feat: getStarted button

---
+++ app/(marketing)/_components/getStarted.tsx
@@ -0,0 +1,10 @@
+
+const GetStarted = () => {
+    return ( 
+        <div className="">
+            <button className="Btn z-50 backdrop-blur-sm">Get Started</button>
+        </div>
+    );
+}
+ 
+export default GetStarted;
