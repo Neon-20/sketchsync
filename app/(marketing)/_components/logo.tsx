bb561989f1fe28827e9164ef2cea892cfd4b4633
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-14 01:37:05 +0530

    feat: logo added

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -3,24 +3,29 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
-    return ( 
-        <div className="transition z-50 ease-in-out duration-300
-        items-center text-transparent bg-clip-text gap-x-2 p-4 flex cursor-pointer">
-            <Image
-            alt = "Logo"
-            src = "/logo.png"
-            width="50"
-            height="50"
-            className="object-cover cursor-pointer flex md:flex"
-            />
-            <Link href="">
-            <p className="z-100 text-2xl text-white font-semibold 
-            text-transparent cursor-pointer backdrop-blur-sm">
-            SketchSync
-            </p>
-            </Link>
-        </div>
-    );
-}
+  return (
+    <div
+      className="transition z-50 ease-in-out duration-300
+        items-center text-transparent bg-clip-text gap-x-2 p-4 flex cursor-pointer"
+    >
+      <Image
+        alt="Logo"
+        src="/logo.png"
+        width="50"
+        height="50"
+        className="object-cover cursor-pointer flex md:flex"
+      />
+      <Link href="">
+        <p
+          className="z-50 text-2xl text-white font-semibold 
+            text-transparent cursor-pointer backdrop-blur-sm
+            hidden md:flex"
+        >
+          SketchSync
+        </p>
+      </Link>
+    </div>
+  );
+};
 
 export default Logo;
