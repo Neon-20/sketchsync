d796c6bd08d40c932a40526fa44665ac5f212fdf
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-17 01:48:18 +0530

    feat:getStarted button

--- app/(marketing)/_components/getStarted.tsx
+++ app/(marketing)/_components/getStarted.tsx
@@ -1,10 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const GetStarted = () => {
-    return ( 
-        <div className="">
-            <button className="Btn z-50 backdrop-blur-sm">Get Started</button>
-        </div>
-    );
-}
- 
+  return (
+    <div className="text-center text-xs text-gray-200 flex flex-col backdrop-blur-sm z-50">
+      {/* <Button
+        variant="default"
+        size="lg"
+        type="button"
+        className="z-50 mt-4 cursor-pointer backdrop-blur-sm font-semibold"
+        asChild
+      >
+        <Link href="/auth/login">Get Started</Link>
+      </Button> */}
+      <button className="Btn mt-4 z-50">Jelly Button</button>
+      {/* <p>Free For all</p> */}
+    </div>
+  );
+};
+
 export default GetStarted;
