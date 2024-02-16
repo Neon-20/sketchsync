19006eace178e0e423596e2e38593fbbc7adffba
Pranav Rajveer <rajveer23pr@gmail.com>
2024-02-17 01:48:26 +0530

    feat:added logo

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div
-      className="transition z-50 ease-in-out duration-300
+    <div className="items-center justify-between flex flex-row flex-wrap">
+      <div
+        className="transition z-50 ease-in-out duration-300
         items-center text-transparent bg-clip-text gap-x-2 p-4 flex cursor-pointer"
-    >
-      <Image
-        alt="Logo"
-        src="/logo.png"
-        width="50"
-        height="50"
-        className="object-cover cursor-pointer flex md:flex"
-      />
-      <Link href="">
+      >
+        <Image
+          alt="Logo"
+          src="/logo.png"
+          width="50"
+          height="50"
+          className="object-cover cursor-pointer flex md:flex"
+        />
         <p
           className="z-50 text-2xl text-white font-semibold 
             text-transparent cursor-pointer backdrop-blur-sm
@@ -23,7 +23,10 @@ const Logo = () => {
         >
           SketchSync
         </p>
-      </Link>
+      </div>
+      <button className="Btn text-sm z-50 mr-4 cursor-pointer" type="button">
+        <Link href="/auth/sign-in">Get Started</Link>
+      </button>
     </div>
   );
 };
